1ccbdbc4e76b2929dd019b1c181f7057256f51c9
Sai Rohit <sairohit11.s@gmail.com>
2023-07-17 19:24:24 +0530

    images update

--- modules/home/edge.tsx
+++ modules/home/edge.tsx
@@ -8,22 +8,27 @@ export default function Edge() {
   const data = [
     {
       spanData: "Professional",
+      src: "training.png",
       para: "Professional approach and commitment.  Understand the Clients requirements thoroughly and equip that only the most suitably qualified candidates are presented to the clients."
     },
     {
       spanData: "Recruitment",
+      src: "recruitment.png",
       para: "Thorough screening and recruitment process to ensure that only the most suitably qualified candidates are presented to the clients."
     },
     {
       spanData: "Partnerships",
+      src: "engagement.png",
       para: "Customized and tailored services to meet the specific, individual needs of the clients."
     },
     {
       spanData: "Customization",
+      src: "customization.png",
       para: "Transparent and long term business partnerships to demonstrate our ongoing commitment to deliver the best quality of service our clients require."
     },
     {
       spanData: "Counselling",
+      src: "about-us.png",
       para: "Help each individual to realize his/her potential and to grow in his/her career path by providing right guidance and counselling."
     }
   ];
@@ -76,7 +81,7 @@ export default function Edge() {
           return (
             <div className={`edge-cards _${index}`} key={index}>
               <div className="img-container">
-                <Image src="/assets/images/hero-img.jpg" alt={item.spanData} fill />
+                <Image src={`/assets/images/${item.src}`} alt={item.spanData} fill />
               </div>
               <div className="text">
                 <h4>
